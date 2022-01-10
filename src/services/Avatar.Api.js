74dcfa541c267850326bc85c57f1a5a86bb517fc
KDSigel <heykarl@gmail.com>
2022-01-10 15:27:10 -0800

    added services file

---
+++ src/services/Avatar.Api.js
@@ -0,0 +1,30 @@
+
+   
+export const getCharacters = async () => {
+    try {
+      const res = await fetch(
+        'https://last-airbender-api.herokuapp.com/api/v1/characters?perPage=20&page=1'
+      );
+      const { results } = await res.json();
+      return {
+        characters: results.map((character) => ({
+          photo: character.photoUrl,
+          name: character.name,
+        }))
+      };
+    } catch (error) {
+      console.error(error.message);
+      return [];
+    }
+  };
+  
+  export const getCharacter = async (id) => {
+    try {
+      const res = await fetch(`https://last-airbender-api.herokuapp.com/api/v1/characters/${id}`);
+      const character = await res.json();
+      return character;
+    } catch (error) {
+      console.error(error.message);
+      return {};
+    }
+  };
